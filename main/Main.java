d233fe935b4ea3c0d077cdc5bbcebfe873cdaa41
Timur Ainetdinov <tainetdinov@itoolabs.com>
2024-04-21 15:25:20 +0300

    Main: in progress

--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,23 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import configs.EatingProbabilityConfig;
+import configs.EntityTemplateConfig;
+import constants.EntityType;
+import entities.Entity;
+import handlers.EntityHandler;
+
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
+        Path probabilityPath = Path.of("resources/EatingProbability.json");
+        Path templatePath = Path.of("resources/EntityTemplates.json");
+        ObjectMapper mapper = new ObjectMapper();
 
+        EatingProbabilityConfig eatingProbabilityConfig = new EatingProbabilityConfig(probabilityPath, new ObjectMapper());
+        EntityHandler handler = new EntityHandler(new EntityTemplateConfig(templatePath, mapper));
+        Entity wolf = handler.createEntity(EntityType.WOLF);
+        Entity wolf2 = handler.createEntity(EntityType.WOLF);
+        Entity grass = handler.createEntity(EntityType.GRASS);
+        System.out.println("Hello");
     }
 }
